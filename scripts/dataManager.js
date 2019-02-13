6c7898ca54a727231fd77288d7b1033f1841c369
Tallon Simmons <[tallon.simmons@icloud.com]>
2019-02-13 17:21:22 -0500

    Removed proxy url

--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -1,12 +1,11 @@
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export default {
   fetchSearchData: (parameter, value) => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?${parameter}=${value}&client_id=fedCzdzOWG`;
-    return fetch(proxyUrl + targetUrl);
+    return fetch(targetUrl);
   },
   fetchPopularGames: () => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=fedCzdzOWG`;
-    return fetch(proxyUrl + targetUrl);
+    return fetch(targetUrl);
   }
 };

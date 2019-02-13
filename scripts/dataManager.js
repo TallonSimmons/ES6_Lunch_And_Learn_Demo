bdef19316a052266b1a61d57b358ee2031c8aa86
Tallon Simmons <[tallon.simmons@icloud.com]>
2019-02-13 17:22:55 -0500

    Requests are now done in no-cors mode

--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -2,10 +2,10 @@
 export default {
   fetchSearchData: (parameter, value) => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?${parameter}=${value}&client_id=fedCzdzOWG`;
-    return fetch(targetUrl);
+    return fetch(targetUrl, {mode: 'no-cors'});
   },
   fetchPopularGames: () => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=fedCzdzOWG`;
-    return fetch(targetUrl);
+    return fetch(targetUrl, {mode: 'no-cors'});
   }
 };

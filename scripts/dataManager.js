208e3c227a8350e0795dfbff5a2702ce49cee4bd
Tallon Simmons <[tallon.simmons@icloud.com]>
2019-02-13 17:25:18 -0500

    Added proxy url back - api requests are broken without it

--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -1,11 +1,12 @@
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export default {
   fetchSearchData: (parameter, value) => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?${parameter}=${value}&client_id=fedCzdzOWG`;
-    return fetch(targetUrl, {mode: 'no-cors'});
+    return fetch(proxyUrl + targetUrl);
   },
   fetchPopularGames: () => {
     const targetUrl = `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=fedCzdzOWG`;
-    return fetch(targetUrl, {mode: 'no-cors'});
+    return fetch(proxyUrl + targetUrl);
   }
 };
